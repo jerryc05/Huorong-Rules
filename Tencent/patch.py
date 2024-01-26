0325310bf2eb3307b4897419e0466fe80daa38a0
jerryc05 <jerryc443@gmail.com>
2024-01-26 00:16:55 -0500

    u

--- Tencent/patch.py
+++ Tencent/patch.py
@@ -44,7 +44,7 @@ with open(Path(__file__).parent / 'tencent-auto.json', 'rb+') as f:
                 'treatment': 0,
             }
         )
-    for p in (R'*\NormalColor\shellstyle.dll', R'*\chrome.exe', R'*\DCIM\*', f"{Path.home()/'Desktop'}\\*"):
+    for p in (R'*\NormalColor\shellstyle.dll', R'*\chrome.exe', R'*\DCIM\*', f"{Path.home()/'Desktop'}\\*", R'*\Adobe\*\Accessibility.api'):
         tencent.append(
             {
                 'res_path': p,
